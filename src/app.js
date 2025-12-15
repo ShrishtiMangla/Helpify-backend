404913f623bfcd1dd7569c9756787575df9b9368
shrishtimangla <shrishtimangla9@gmail.com>
2025-12-15 22:24:01 +0530

    fixed code

--- src/app.js
+++ src/app.js
@@ -2,31 +2,32 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-// Connect DB (safe for Vercel + local)
+// Connect MongoDB
 connectDB();
 
-app.use(cors());
-app.use(express.json());
-
+// Middleware
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://meek-cactus-7a5d94.netlify.app/"
+      "https://meek-cactus-7a5d94.netlify.app"
     ],
     credentials: true
   })
 );
 
+app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running with MongoDB" });
+  res.status(200).json({
+    message: "Backend is running with MongoDB"
+  });
 });
 
 export default app;
