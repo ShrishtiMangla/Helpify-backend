8a236a8ec701c93c2bdb79a2db8c3144cb416dfc
shrishtimangla <shrishtimangla9@gmail.com>
2025-12-15 22:19:09 +0530

    connected frontend and backend

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +13,17 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://meek-cactus-7a5d94.netlify.app/"
+    ],
+    credentials: true
+  })
+);
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is running with MongoDB" });
